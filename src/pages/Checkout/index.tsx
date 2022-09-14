f4f3a26e3cee0794909619b57e0e2c526f732259
LSantoni <lizandra.santoni@gmail.com>
2022-09-14 18:23:07 -0300

    Iniciar tela de checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,60 @@
+import { CheckoutContainer, ClienteData, CoffeeData } from "./styles";
+
+import { MapPinLine, MapPin, CurrencyDollar, CreditCard, Money, Bank } from "phosphor-react"
+
 export function Checkout() {
-  return <h1>Checkout</h1>
+  return (
+    <CheckoutContainer>
+      <ClienteData>
+        <header>Complete seu pedido</header>
+        <div>
+          <header>
+            <MapPinLine size={20} />
+            <p>Endereço de Entrega</p>
+            <p>Informe o endereço onde deseja receber seu pedido</p>
+          </header>
+          <form action="">
+            <input type="text" placeholder="CEP" />
+            <input type="text" />
+            <div>
+              <input type="text" />
+              <input type="text" />
+            </div>
+            <div>
+              <input type="text" />
+              <input type="text" />
+              <input type="text" />
+            </div>
+          </form>
+        </div>
+        <div>
+          <header>
+            <img src="" alt="" />
+          </header>
+          <div></div>
+        </div>
+      </ClienteData>
+      <CoffeeData>
+        <header>Cafés selecionados</header>
+        <div>
+          <CreditCard size={20} />
+          <input type="checkbox" name="" id="" />
+          CARTÃO DE CRÉDITO
+        </div>
+        <div>
+          <Bank size={20} />
+          <input type="checkbox" name="" id="" />
+          CARTÃO DE DÉBITO
+        </div>
+        <div>
+          <Money size={20} />
+          <input type="checkbox" name="" id="" />
+          DINHEIRO
+        </div>
+        <div>
+          <button>Confirmar Pedido</button>
+        </div>
+      </CoffeeData>
+    </CheckoutContainer>
+  )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CheckoutContainer = styled.main`
+
+`
+
+export const ClienteData = styled.div`
+
+`
+
+export const CoffeeData = styled.div`
+  
+`
